48660a843fe37499f29754e01c68a9bca50a6ee1
Sunny Ripert <sunny@sunfox.org>
2025-04-16 20:35:07 +0200

    Add Poland 🇵🇱 (#8)

---
+++ spec/tin_valid/poland_tin_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe TinValid::PolandTin do
+  describe "#valid?" do
+    valid_values = [
+      ["2234567895", nil],
+      ["02070803628", Date.new(1902, 7, 8)],
+    ]
+
+    invalid_values = [
+      ["223456789", nil],
+      ["22345678959", nil],
+      ["2234567899", nil],
+      ["02070803628", Date.new(1902, 7, 9)],
+      [nil, nil],
+      ["", nil],
+    ]
+
+    valid_values.each do |(tin, birth_date)|
+      context(
+        "with valid #{tin.inspect} and birth date #{birth_date.inspect}",
+      ) do
+        it { expect(described_class.new(tin:, birth_date:).valid?).to be(true) }
+      end
+    end
+
+    invalid_values.each do |(tin, birth_date)|
+      context(
+        "with invalid #{tin.inspect} and birth date #{birth_date.inspect}",
+      ) do
+        it do
+          expect(described_class.new(tin:, birth_date:).valid?).to be(false)
+        end
+      end
+    end
+  end
+end
