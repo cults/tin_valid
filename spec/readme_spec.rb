7dde868b3e58e8c631e0f6e22bbc7448925e2318
Sunny Ripert <sunny@sunfox.org>
2025-05-14 17:58:14 +0200

    Remove unused `let`

--- spec/readme_spec.rb
+++ spec/readme_spec.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe "README" do
-  let(:readme) { File.read(readme_path) }
-  let(:readme_path) { File.join(__dir__, "../README.md") }
-
   readme = File.read(File.join(__dir__, "../README.md"))
   valid_codes = readme.scan(/```rb\n(.*?)\n```/m).flatten
   valid_codes.each do |code|
